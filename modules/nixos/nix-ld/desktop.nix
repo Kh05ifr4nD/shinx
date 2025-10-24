d99fd641f6ea16905469fbbcfdebb98e92270a08
Kh05ifr4nD <meandSSH0219@gmail.com>
2025-10-24 16:36:24 +0800

    refactor(nix-ld): 桌面运行时去除 GTK 组件（gtk2/libappindicator-gtk2）以契合 Wayland+niri 方向\n\nSigned-off-by: Kh05ifr4nD <meandSSH0219@gmail.com>
    Signed-off-by: Kh05ifr4nD <meandSSH0219@gmail.com>

--- modules/nixos/nix-ld/desktop.nix
+++ modules/nixos/nix-ld/desktop.nix
@@ -25,10 +25,8 @@
       gst_all_1.gst-plugins-base
       gst_all_1.gst-plugins-ugly
       gst_all_1.gstreamer
-      gtk2
       harfbuzz
       keyutils.lib
-      libappindicator-gtk2
       libcaca
       libcanberra
       libclang.lib
