dcab1f770bbf05d3a04055afc72c84398a677167
Kh05ifr4nD <meandSSH0219@gmail.com>
2025-10-27 17:32:15 +0800

    refactor(hardware): 简化模块，移除自定义选项；默认启用可再分发固件并按其启用 microcode
    Signed-off-by: Kh05ifr4nD <meandSSH0219@gmail.com>

--- modules/nixos/hardware/default.nix
+++ modules/nixos/hardware/default.nix
@@ -1,36 +1,13 @@
+{ lib, config, ... }:
 {
-  flake,
-  lib,
-  config,
-  pkgs,
-  ...
-}:
-let
-  inherit (flake) inputs;
-  cfg = config.modules.hardware;
-in
-{
-  options.modules.hardware = {
-    coolercontrol.enable = lib.mkEnableOption "Enable CoolerControl";
-    microcode.auto = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = "Automatically enable CPU microcode updates based on firmware availability.";
+  config = {
+    hardware = {
+      cpu = {
+        amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+        intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+      };
+      enableRedistributableFirmware = lib.mkDefault true;
     };
+    services.fstrim.enable = lib.mkDefault true;
   };
-
-  config = lib.mkMerge [
-    (lib.mkIf cfg.coolercontrol.enable {
-      programs.coolercontrol.enable = true;
-    })
-
-    (lib.mkIf cfg.microcode.auto {
-      hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-      hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-    })
-
-    {
-      services.fstrim.enable = lib.mkDefault true;
-    }
-  ];
 }
