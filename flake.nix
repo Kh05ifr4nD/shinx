c22f5dd8c8e19f5f84698be3e2b3c892bada863f
Kh05ifr4nD <meandSSH0219@gmail.com>
2025-10-27 21:35:06 +0800

    chore(flake): 添加 niri.cachix 二进制缓存
    Signed-off-by: Kh05ifr4nD <meandSSH0219@gmail.com>

--- flake.nix
+++ flake.nix
@@ -46,10 +46,12 @@
       "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://nix-community.cachix.org"
       "https://cache.nixos.org"
+      "https://niri.cachix.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
     ];
   };
   outputs =
